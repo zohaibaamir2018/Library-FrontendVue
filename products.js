1f27b412ad6e92df2cdf4bb96af08745451f8c87
zohaibaamir2018 <42832127+zohaibaamir2018@users.noreply.github.com>
2024-12-13 17:19:15 +0400

    Frontend Vue.js

---
+++ products.js
@@ -0,0 +1,92 @@
+export const products = [
+    {
+        id: 1,
+        name: "Arcade Gaming",
+        image: "Images/Arcade Gaming.jpg",
+        price: 50.00,
+        availableInventory: 5,
+        location: "Gaming Zone",
+        rating: 4
+    },
+    {
+        id: 2,
+        name: "Art and Craft",
+        image: "Images/Art and Craft.jpg",
+        price: 60.00,
+        availableInventory: 5,
+        location: "Art Room",
+        rating: 3
+    },
+    {
+        id: 3,
+        name: "Basketball",
+        image: "Images/Basketball.jpg",
+        price: 40.00,
+        availableInventory: 5,
+        location: "Basketball Court",
+        rating: 5
+    },
+    {
+        id: 4,
+        name: "Chemistry Lab",
+        image: "Images/Chemistry.webp",
+        price: 70.00,
+        availableInventory: 5,
+        location: "Lab 1",
+        rating: 3
+    },
+    {
+        id: 5,
+        name: "Gymnasium",
+        image: "Images/Gymnesium.jpg",
+        price: 80.00,
+        availableInventory: 5,
+        location: "Fitness Center",
+        rating: 4
+    },
+    {
+        id: 6,
+        name: "Kickboxing",
+        image: "Images/Kickboxing.jpg",
+        price: 90.00,
+        availableInventory: 5,
+        location: "Martial Arts Room",
+        rating: 5
+    },
+    {
+        id: 7,
+        name: "Music",
+        image: "Images/Music.jpg",
+        price: 75.00,
+        availableInventory: 5,
+        location: "Music Hall",
+        rating: 4
+    },
+    {
+        id: 8,
+        name: "Snooker",
+        image: "Images/Snooker.jpg",
+        price: 45.00,
+        availableInventory: 5,
+        location: "Gaming Lounge",
+        rating: 4
+    },
+    {
+        id: 9,
+        name: "Physics Lab",
+        image: "Images/Physics.jpg",
+        price: 55.00,
+        availableInventory: 5,
+        location: "Lab 2",
+        rating: 3
+    },
+    {
+        id: 10,
+        name: "Robotics",
+        image: "Images/Robotics.jpg",
+        price: 50.00,
+        availableInventory: 5,
+        location: "Robotics Lab",
+        rating: 4
+    }
+];
